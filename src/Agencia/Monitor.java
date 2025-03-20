f91dcf9f6b23e1a64bdeb56e8435bc0d51f9bbd4
Sofia Castro <116833245+Sofi268@users.noreply.github.com>
2025-03-20 18:03:23 -0300

    Update Monitor.java

--- src/Agencia/Monitor.java
+++ src/Agencia/Monitor.java
@@ -1,22 +1,23 @@
 package Agencia;
 
 public class Monitor implements MonitorInterface{
-	private RedDePetri red;
-	public Monitor() {
-		 red = new RedDePetri();
+	private RedDePetri redPetri;
+	public Monitor(RedDePetri red) {
+		 redPetri = red;
 	}
 	
 	@Override
 	public boolean fireTransition(int transition) {
-		if (red.isSensible(transition)) {
+		if (redPetri.isSensible(transition)) {
 			System.out.println("La transicion se puede disparar");
-			red.disparar(transition);
+			redPetri.disparar(transition);
 			
 			System.out.println("La transicion fue disparada");
 			return true;
 		}
-		System.out.println("No es posible disparar la transicion");
+		System.out.println("No es posible disparar la transicion " + transition);
 		return false;
 	}
 
 }
+
