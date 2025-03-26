f970d4be739af3364a2405a1560c093603ebed67
Sofia Castro <116833245+Sofi268@users.noreply.github.com>
2025-03-26 11:13:20 -0300

    Delete src/Agencia directory

--- src/Agencia/Constantes.java
+++
@@ -1,6 +0,0 @@
-package Agencia;
-public class Constantes {
-    public static final Integer cantidadGestoresReserva; //encargados de la reserva.
-    public static final Integer cantidadGestoresCaja;  //encargados de la confirmacion/pago o de la cancelacion.
-
-}
