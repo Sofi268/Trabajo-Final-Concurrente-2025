2afa456687fb62a1f3452032f664db4b3814bb38
Sofia Castro <116833245+Sofi268@users.noreply.github.com>
2025-04-05 11:58:17 -0300

    Update Politicas.java

--- src/Agencia/Politicas.java
+++ src/Agencia/Politicas.java
@@ -6,15 +6,44 @@ package Agencia;
 
 public class Politicas {
     private static Politicas uniqueInstance;
-    
-    /**
+    private static String nombre;
+    private static int cantDisparosT2;
+    private static int cantDisparosT3;
+    private static int cantDisparosT6;
+    private static int cantDisparosT7;
+    private int tipoPolitica;
+
+    private boolean permitirT2;
+    private boolean permitirT3;
+    private boolean permitirT6;
+    private boolean permitirT7;
+
+     /**
      * @brief Constructor vacio
-     */
+     */    
     public Politicas() {}
+    
     /**
      * @brief Aplicando patron de diseño singleton nos aseguramos que solo haya 1 politica activa.
      * @return Políticas
      */
+    public Politicas(String politica) {
+        nombre = politica;
+        cantDisparosT2 = 0;
+        cantDisparosT3 = 0;
+        cantDisparosT6 = 0;
+        cantDisparosT7 = 0;
+
+        permitirT2 = true;
+        permitirT3 = false;
+        permitirT6 = true;
+        permitirT7 = false;
+
+        if (politica.equals("Balanceada")) iniciarPoliticaBalanceada();
+        else if (politica.equals("Prioridad")) iniciarPoliticaPrioridad();
+        else System.out.println("No existe esa politica");
+    }
+
     public static Politicas getInstance(String politica){
         if (uniqueInstance == null){
             uniqueInstance = new Politicas(politica);
@@ -23,32 +52,80 @@ public class Politicas {
         }
         return uniqueInstance;
     }
-    /**
-     * @brief Determina la creacion de la politica elegida
-     * @param politica a implementar
-     */
-    public Politicas(String politica) {
-    	if(politica.equals("Balanceada")) iniciarPoliticaBalanceada();
-    	else {
-    		if(politica.equals("Prioridad")) iniciarPoliticaBalanceada();
-    		else {
-    			System.out.println("No existe esa politica");
-    		}
-    	}
-    }
 
     /**
      * @brief Crea instancia de Politica Balanceada
      */
-    public void iniciarPoliticaBalanceada(){
-        // TODO
+    private void iniciarPoliticaBalanceada(){
+        tipoPolitica = 1;
     }
     
     /**
      * @brief Crea instancia de Politica con Prioridad
      */
-     public void iniciarPoliticaPrioridad(){
-         // TODO
-     }
-    
+    private void iniciarPoliticaPrioridad(){
+        tipoPolitica = 2;
+    }
+
+    public boolean sePuedeDisparar(int t) {
+        switch (t) {
+            case 2: return manejarT2();
+            case 3: return manejarT3();
+            case 6: return manejarT6();
+            case 7: return manejarT7();
+            default: return true;
+        }
+    }
+
+    private boolean manejarT2() {
+        if (!permitirT2) return false;
+        cantDisparosT2++;
+
+        if (tipoPolitica == 1 && cantDisparosT2 >= cantDisparosT3) {
+            permitirT3 = true;
+            permitirT2 = false;
+        }
+        if (tipoPolitica == 2 && cantDisparosT2 % 3 == 0) { // 75%
+            permitirT3 = true;
+            permitirT2 = false;
+        }
+        return true;
+    }
+
+    private boolean manejarT3() {
+        if (!permitirT3) return false;
+        cantDisparosT3++;
+
+        permitirT2 = true;
+        permitirT3 = false;
+        return true;
+    }
+
+    private boolean manejarT6() {
+        if (!permitirT6) return false;
+        cantDisparosT6++;
+
+        if (tipoPolitica == 1 && cantDisparosT6 >= cantDisparosT7) {
+            permitirT7 = true;
+            permitirT6 = false;
+        }
+        if (tipoPolitica == 2 && cantDisparosT6 % 4 == 0) { // 80%
+            permitirT7 = true;
+            permitirT6 = false;
+        }
+        return true;
+    }
+
+    private boolean manejarT7() {
+        if (!permitirT7) return false;
+        cantDisparosT7++;
+
+        permitirT6 = true;
+        permitirT7 = false;
+        return true;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }    
 }
