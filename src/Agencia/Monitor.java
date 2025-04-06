401105e50dcc6780fd4717caece75409061f3364
Efrain Veliz <128051011+efrainveliz2023@users.noreply.github.com>
2025-04-05 22:03:48 -0300

    Update Monitor.java

--- src/Agencia/Monitor.java
+++ src/Agencia/Monitor.java
@@ -1,121 +1,181 @@
 package Agencia;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Semaphore;
 
-public class Estadisticas implements Runnable {
-    private ArrayList<Integer> transicionesDisparadas;
-    private boolean listo;
-    private static RedDePetri red;
-    private static int cantidadInvariantes;
-	private int porcentajeT2;
-	private int porcentajeT3;
-    private int porcentajeT6;
-	private int porcentajeT7;
-	
+public class Monitor implements MonitorInterface {
+    private static Monitor uniqueInstance;
+    private static RedDePetri rdp = RedDePetri.getInstance();
+    private static Politicas politica;
+    private static Semaphore mutex = new Semaphore(1, true);
+    private static Semaphore colaEntrada = new Semaphore(1, true);
+    private static Semaphore[] colasCondicion;
+    private static int[] colasConHilos;
+    private static int ultimoOrden;
 
-    public Estadisticas(RedDePetri redPetri) {
-        transicionesDisparadas = new ArrayList<>();
-        listo = false;
-        red = redPetri;
-        cantidadInvariantes = 0;
-        porcentajeT2 = 0;
-        porcentajeT3 = 0;
-        porcentajeT6 = 0;
-        porcentajeT7 = 0;
+    public Monitor() {
     }
 
-    @Override
-    public void run() {
-        System.out.println("Hilo de estadísticas iniciado");
-        try (FileWriter file = new FileWriter("log1.txt");
-             PrintWriter pw = new PrintWriter(file)) {
-            
-            pw.printf("************** Inicio del registro: %s *********\n", new Date());
-            pw.flush();
+    /* Aplico patron Singleton ...
+    * */
+    public static Monitor getInstance(){
+        if(uniqueInstance == null){
+            uniqueInstance = new Monitor();
+            startMonitor();
+        }
+        return uniqueInstance;
+    }
 
-            while (true) {
-                try {
-                    TimeUnit.MILLISECONDS.sleep(500);
-                } catch (InterruptedException e) {
-                    System.out.println("Hilo interrumpido");
-                    Thread.currentThread().interrupt();
-                    return;
+    /* startMonitor:
+    * */
+    public static void startMonitor(){
+    	politica = Politicas.getInstance("Balanceada");
+        colasCondicion = new Semaphore[rdp.getTransiciones()];
+        colasConHilos = new int[rdp.getTransiciones()];
+        ultimoOrden = -1;
+        for (int i = 0; i < rdp.getTransiciones(); i++) {
+            colasCondicion[i] = new Semaphore(0);
+            colasConHilos[i] = 0; // Inicializa todas las colas como vacías
+        }
+        System.out.println("Monitor inicializado con " + rdp.getTransiciones() + " transiciones.");
+    }
+    // -----------------------------------------------------------------------------------------------------------------
+    // logica de monitor.
+    @Override
+    public boolean fireTransition(int transition) {
+        return fire(transition);
+    }
+    private static boolean fire(int transition) {
+        try {
+            colaEntrada.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        puertaMonitor(transition);
+        return false;
+    }
+    private static void puertaMonitor(Integer transition){
+        System.out.println("Intentando agarrar mutex: " + transition);
+        agarrarMutex();
+        System.out.println("Mutex adquirido para transición: " + transition);
+        entrarMonitor(transition);
+    }
+    //-------------------------------------------------------------------------------------------------------------
+    /**
+     * Se intenta agarrar el mutex:
+     */
+    private static void agarrarMutex(){
+        try {
+            mutex.acquire();
+        }
+        catch (InterruptedException e) {
+            System.out.println("Error en fireTransition: " + e.getMessage());
+            //System.exit(1);
+        }
+    }
+    private static void liberarMutex() {
+        if (mutex.availablePermits() == 0) {
+            System.out.println("Liberando mutex.");
+            mutex.release();
+        }
+    }
+
+    //-------------------------------------------------------------------------------------------------------------
+    private static boolean entrarMonitor(int t) {
+        if (rdp.isSensible(t)){  // si esta sensibilizada la transicion y la politica lo permite
+            System.out.println("Entrando al monitor con transición: " + t);
+            if(t==2 || t==3 || t==6 || t==7) {
+                if(!politica.sePuedeDisparar(t)) {
+                    System.out.println("Transición " + t + " no se puede disparar todavia por la politica "+ politica.getNombre() + ". Derivando a cola.");
+                    derivarACola(t);
+                    return false;
                 }
-                
-                actualizarDisparos();
-                actualizarStats();
-                imprimir(pw);
-//                if (comprobarInvariantes()) {
-//                    listo = true;
-//                }
             }
-            //pw.printf("Finalizando registro. Se han disparado %d invariantes.\n", cantidadInvariantes);
-            //pw.flush();
-        } catch (IOException e) {
+            System.out.println("Entrando al monitor con transición: " + t);
+            ejecutarDisparo(t);
+
+            int tSensible = hayColaCondicion();
+            if (tSensible >= 0) {
+                System.out.println("Señalizando siguiente transición en cola: " + tSensible);
+                señalizarSig(tSensible);
+            } else {
+                System.out.println("No hay transiciones en cola. Liberando mutex.");
+                if (colaEntrada.hasQueuedThreads()) {
+                    colaEntrada.release();
+                }
+                liberarMutex();
+            }
+            return true;
+        }else{
+            System.out.println("Transición " + t + " no es sensible. Derivando a cola.");
+            derivarACola(t);
+            return false;
+        }
+    }
+    private static void derivarACola(int t) {
+        try {
+            System.out.println("Colocando transición " + t + " en cola de condición.");
+            colasConHilos[t] ++;
+            liberarMutex();
+            colaEntrada.release();
+            colasCondicion[t].acquire();
+            System.out.println("Hilo de transición " + t + " liberado de la cola.");
+            entrarMonitor(t);
+        } catch (InterruptedException e) {
+            System.out.println("No se pudo derivar a la cola de condición");
             e.printStackTrace();
         }
     }
-
-    public void actualizarDisparos() {
-        transicionesDisparadas = new ArrayList<>(red.getTransicionesDisparadas());
+    private static void señalizarSig(int tSensible) {
+        System.out.println("Liberando hilo en cola de transición " + tSensible);
+        colasCondicion[tSensible].release();
+        colasConHilos[tSensible]--; // se libera la cola de condicion.
     }
-
-    public void actualizarStats() {
-    	int disparosT2 = red.getDisparosT2();
-    	int disparosT3 = red.getDisparosT3();
-    	int disparosT6 = red.getDisparosT6();
-    	int disparosT7 = red.getDisparosT7();
-    	
-    	int totalT23 = disparosT2 + disparosT3;
-    	int totalT67 = disparosT6 + disparosT7;
-
-    	porcentajeT2 = totalT23 == 0 ? 0 : (disparosT2 * 100) / totalT23;
-    	porcentajeT3 = totalT23 == 0 ? 0 : (disparosT3 * 100) / totalT23;
-    	porcentajeT6 = totalT67 == 0 ? 0 : (disparosT6 * 100) / totalT67;
-    	porcentajeT7 = totalT67 == 0 ? 0 : (disparosT7 * 100) / totalT67;
-
-    }
-    
-    public void imprimir(PrintWriter pw) {
-        if (transicionesDisparadas == null ) {
-        	if(transicionesDisparadas.isEmpty()) {
-        		pw.printf("No hay transiciones disparadas aún.\n");
-        	}
-     
-        } else {
-        	
-            pw.printf("Transiciones disparadas: %s\n", transicionesDisparadas.toString());
-            /*pw.printf("\n La transicion T2 fue disparada " + porcentajeT2 + "% de las veces");
-            // System.out.println("El gestor 1 gestiono el " + porcentajeT2 + "% de las reservas");	
-           pw.printf("\n La transicion T3 fue disparada " + porcentajeT3 + "% de las veces");
-        // System.out.println("El gestor 2 gestiono el " + porcentajeT3 + "% de las reservas");
-           pw.printf("\n La transicion T6 fue disparada " + porcentajeT6 + "% de las veces");
-         //System.out.println("El agente aprobo el " + porcentajeT6 + "% de las reservas");
-           pw.printf("\n La transicion T7 fue disparada " + porcentajeT7 + "% de las veces");
-         //System.out.println("El agente rechazo el " + porcentajeT7 + "% de las reservas");*/
-            pw.printf("La transicion T2 fue disparada %d%% de las veces\n", porcentajeT2);
-            pw.printf("La transicion T3 fue disparada %d%% de las veces\n", porcentajeT3);
-            pw.printf("La transicion T6 fue disparada %d%% de las veces\n", porcentajeT6);
-            pw.printf("La transicion T7 fue disparada %d%% de las veces\n", porcentajeT7);
-
+    /**
+     * Dispara la transicion dentro de la red e incrementa la cantidad de invariantes en la clase Politicas, luego
+     * chequea si hay Transiciones en la cola de concicion, si hay la dispara, incrementa la cantidad de invariantes en
+     * la clase Politica y saca a la transicion de la cola de Condicion
+     * @param t transición a disparar
+     */
+    private static void ejecutarDisparo(Integer t){
+        boolean disparo = rdp.disparar(t);
+        if (!disparo || tieneMarcadoNegativo()) {
+            System.out.println("Error: Marcado negativo detectado tras disparar T" + t);
+            System.out.println("No se pudo disparar transición " + t + ". Derivando a cola.");
+            derivarACola(t);
+            System.exit(1);
         }
-        pw.flush();
+        System.out.println("Transición " + t + " disparada exitosamente.");
+    }
+    private static boolean tieneMarcadoNegativo() {
+        for (Integer valor : rdp.getMarcadoActual()) {
+            if (valor < 0) {
+                System.out.println("Marcado negativo detectado.");
+                return true;
+            }
+        }
+        return false;
     }
 
-    public boolean comprobarInvariantes() {
-        return getCantInvariantes() >= 186;
+    private static int hayColaCondicion() {
+        int n = 12;
+        int inicio = (ultimoOrden + 1) % n;
+        System.out.println("Buscando transiciones en cola de condición.");
+        for (int i = inicio; i < n; i++) {
+            if ((colasConHilos[i]>0) && rdp.isSensible(i)) {
+                ultimoOrden = i;
+                System.out.println("Encontrada transición en cola: " + i);
+                return i;
+            }
+        }
+        for (int i = 0; i < inicio; i++) {
+            if ((colasConHilos[i]>0) && rdp.isSensible(i)) {
+                ultimoOrden = i;
+                System.out.println("Encontrada transición en cola: " + i);
+                return i;
+            }
+        }
+        System.out.println("No se encontraron transiciones en cola de condición.");
+        return -1;
     }
-
-    public int getCantInvariantes() {
-        int invariantes = 0;
-        // TODO: Implementar lógica para contar invariantes
-        return invariantes;
-    }
-    
-
 }
+
