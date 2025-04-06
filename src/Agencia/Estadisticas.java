eeda868b544d2decca92c4b9f17145cb8f608d02
Efrain Veliz <128051011+efrainveliz2023@users.noreply.github.com>
2025-04-05 22:04:16 -0300

    Update Estadisticas.java

--- src/Agencia/Estadisticas.java
+++ src/Agencia/Estadisticas.java
@@ -10,27 +10,37 @@ import java.util.concurrent.TimeUnit;
 public class Estadisticas implements Runnable {
     private ArrayList<Integer> transicionesDisparadas;
     private boolean listo;
-    private static RedDePetri red = RedDePetri.getInstance();
+    private static RedDePetri red;
     private static int cantidadInvariantes;
+	private int porcentajeT2;
+	private int porcentajeT3;
+    private int porcentajeT6;
+	private int porcentajeT7;
+	
 
-    public Estadisticas() {
+    public Estadisticas(RedDePetri redPetri) {
         transicionesDisparadas = new ArrayList<>();
         listo = false;
+        red = redPetri;
         cantidadInvariantes = 0;
+        porcentajeT2 = 0;
+        porcentajeT3 = 0;
+        porcentajeT6 = 0;
+        porcentajeT7 = 0;
     }
 
     @Override
     public void run() {
         System.out.println("Hilo de estadísticas iniciado");
-        try (FileWriter file = new FileWriter("log.txt");
+        try (FileWriter file = new FileWriter("log1.txt");
              PrintWriter pw = new PrintWriter(file)) {
             
             pw.printf("************** Inicio del registro: %s *********\n", new Date());
             pw.flush();
 
-            while (!listo) {
+            while (true) {
                 try {
-                    TimeUnit.MILLISECONDS.sleep(50);
+                    TimeUnit.MILLISECONDS.sleep(500);
                 } catch (InterruptedException e) {
                     System.out.println("Hilo interrumpido");
                     Thread.currentThread().interrupt();
@@ -38,13 +48,14 @@ public class Estadisticas implements Runnable {
                 }
                 
                 actualizarDisparos();
+                actualizarStats();
                 imprimir(pw);
-                if (comprobarInvariantes()) {
-                    listo = true;
-                }
+//                if (comprobarInvariantes()) {
+//                    listo = true;
+//                }
             }
-            pw.printf("Finalizando registro. Se han disparado %d invariantes.\n", cantidadInvariantes);
-            pw.flush();
+            //pw.printf("Finalizando registro. Se han disparado %d invariantes.\n", cantidadInvariantes);
+            //pw.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -54,7 +65,22 @@ public class Estadisticas implements Runnable {
         transicionesDisparadas = new ArrayList<>(red.getTransicionesDisparadas());
     }
 
+    public void actualizarStats() {
+    	int disparosT2 = red.getDisparosT2();
+    	int disparosT3 = red.getDisparosT3();
+    	int disparosT6 = red.getDisparosT6();
+    	int disparosT7 = red.getDisparosT7();
+    	
+    	int totalT23 = disparosT2 + disparosT3;
+    	int totalT67 = disparosT6 + disparosT7;
 
+    	porcentajeT2 = totalT23 == 0 ? 0 : (disparosT2 * 100) / totalT23;
+    	porcentajeT3 = totalT23 == 0 ? 0 : (disparosT3 * 100) / totalT23;
+    	porcentajeT6 = totalT67 == 0 ? 0 : (disparosT6 * 100) / totalT67;
+    	porcentajeT7 = totalT67 == 0 ? 0 : (disparosT7 * 100) / totalT67;
+
+    }
+    
     public void imprimir(PrintWriter pw) {
         if (transicionesDisparadas == null ) {
         	if(transicionesDisparadas.isEmpty()) {
@@ -62,7 +88,21 @@ public class Estadisticas implements Runnable {
         	}
      
         } else {
+        	
             pw.printf("Transiciones disparadas: %s\n", transicionesDisparadas.toString());
+            /*pw.printf("\n La transicion T2 fue disparada " + porcentajeT2 + "% de las veces");
+            // System.out.println("El gestor 1 gestiono el " + porcentajeT2 + "% de las reservas");	
+           pw.printf("\n La transicion T3 fue disparada " + porcentajeT3 + "% de las veces");
+        // System.out.println("El gestor 2 gestiono el " + porcentajeT3 + "% de las reservas");
+           pw.printf("\n La transicion T6 fue disparada " + porcentajeT6 + "% de las veces");
+         //System.out.println("El agente aprobo el " + porcentajeT6 + "% de las reservas");
+           pw.printf("\n La transicion T7 fue disparada " + porcentajeT7 + "% de las veces");
+         //System.out.println("El agente rechazo el " + porcentajeT7 + "% de las reservas");*/
+            pw.printf("La transicion T2 fue disparada %d%% de las veces\n", porcentajeT2);
+            pw.printf("La transicion T3 fue disparada %d%% de las veces\n", porcentajeT3);
+            pw.printf("La transicion T6 fue disparada %d%% de las veces\n", porcentajeT6);
+            pw.printf("La transicion T7 fue disparada %d%% de las veces\n", porcentajeT7);
+
         }
         pw.flush();
     }
@@ -76,4 +116,6 @@ public class Estadisticas implements Runnable {
         // TODO: Implementar lógica para contar invariantes
         return invariantes;
     }
+    
+
 }
