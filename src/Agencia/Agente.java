25b71f450ba59da22f96031b3e0c9dc983aa7f6a
Efrain Veliz <128051011+efrainveliz2023@users.noreply.github.com>
2025-04-07 09:57:40 -0300

    Update Agente.java

--- src/Agencia/Agente.java
+++ src/Agencia/Agente.java
@@ -4,6 +4,8 @@
  */
 package Agencia;
 
+import java.util.concurrent.TimeUnit;
+
 public class Agente implements Runnable {
 	private Monitor monitor;
 	private int[] transiciones1 = {6,9,10};	
@@ -17,22 +19,24 @@ public class Agente implements Runnable {
 	
 	@Override
 	public void run() {
-		boolean estado1;
-		boolean estado2;
+		//boolean estado1;
+		//boolean estado2;
 		if(tipo==1) {
 			while(true) {
 				for(int x: transiciones1) {
-					do{
-						estado1 = monitor.fireTransition(x);
-					}while(!estado1);
+					//do{
+						monitor.fireTransition(x);
+						
+					//}while(!estado1);
 				}
 			}
 		}else {
 			while(true) {
 				for(int x: transiciones2) {
-					do{
-						estado2 = monitor.fireTransition(x);
-					}while(!estado2);
+					//do{
+						monitor.fireTransition(x);
+						
+					//}while(!estado2);
 				}
 			}
 		}
