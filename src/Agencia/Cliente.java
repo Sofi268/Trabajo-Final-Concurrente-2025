fb972180738ea078de751e9003444cc05388c161
Efrain Veliz <128051011+efrainveliz2023@users.noreply.github.com>
2025-04-07 09:57:03 -0300

    Update Cliente.java

--- src/Agencia/Cliente.java
+++ src/Agencia/Cliente.java
@@ -4,6 +4,8 @@
  */
 package Agencia;
 
+import java.util.concurrent.TimeUnit;
+
 public class Cliente implements Runnable{
 	private Monitor monitor;
 	private int[] transicionesEntrada= {0,1};	
@@ -17,24 +19,45 @@ public class Cliente implements Runnable{
 
 	@Override
 	public void run() {
-		boolean estado1;
-		boolean estado2;
+		//boolean estado1;
+		//boolean estado2;
 		if(tipo==1) {
 			while(true) {
 				for(int x: transicionesEntrada) {
-					do{
-						estado1 = monitor.fireTransition(x);
-					}while(!estado1);
+					//do{
+						monitor.fireTransition(x);
+						
+						getIn();
+					//}while(!estado1);
 				}
 			}
 		}else {
 			while(true) {
 				for(int x: transicionesSalida) {
-					do{
-						estado2 = monitor.fireTransition(x);
-					}while(!estado2);
+					//do{
+						monitor.fireTransition(x);
+						getOut();
+					//}while(!estado2);
 				}
 			}
 		}
+	}
+	private void getIn() {
+		try {
+			System.out.printf("[Cliente entrante] Transiciones de entrada completadas. Durmiendo hilo:  %d \n", Thread.currentThread().getId());
+			TimeUnit.MILLISECONDS.sleep(1);
+			System.out.printf("[Cliente entrante] Despierta. Volviendo a intentar desde hilo: %d \n", Thread.currentThread().getId());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+	}
+	private void getOut() {
+		try {
+			System.out.printf("[Cliente saliente] Transiciones de entrada completadas. Durmiendo hilo:  %d \n", Thread.currentThread().getId());
+			TimeUnit.MILLISECONDS.sleep(2);
+			System.out.printf("[Cliente saliente] Despierta. Volviendo a intentar desde hilo: %d \n", Thread.currentThread().getId());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 	}
 }
