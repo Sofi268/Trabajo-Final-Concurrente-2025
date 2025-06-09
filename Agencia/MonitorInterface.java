6ccd1fe56094b6e960712769cbbd6d92ad4df4dc
Sofia Castro <116833245+Sofi268@users.noreply.github.com>
2025-06-09 11:51:30 -0300

    Delete Agencia directory

--- Agencia/MonitorInterface.java
+++
@@ -1,9 +0,0 @@
-/**
- * @file MonitorInterface.java
- * @brief Interfaz que exige la definicion del disparo del monitor
- */
-package Agencia;
-
-public interface MonitorInterface {
-	boolean fireTransition(int transition);
-}
