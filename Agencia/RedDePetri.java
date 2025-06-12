cef7ebb086f5f25a1aefe4a51ecb8d4f3454a7df
Sofia Castro <116833245+Sofi268@users.noreply.github.com>
2025-06-12 11:08:18 -0300

    Update RedDePetri.java

--- Agencia/RedDePetri.java
+++ Agencia/RedDePetri.java
@@ -7,11 +7,30 @@ import java.util.ArrayList;
 public class RedDePetri {
     private static RedDePetri uniqueInstance;  //Para implementacion de patron de diseño singleton.
     public Estadisticas estadisticas = Estadisticas.getInstance(); // Instancia de estadísticas para monitorear el estado de la red de Petri.
-
+    
+    //veririfarr --- inicio
+    private static final Integer[] marcadoInicial = {5, 1, 0, 0, 5, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0};
     private static Integer[] marcadoActual;
+    private static Integer[][] matrizIncidencia;
     private static Integer[] transicionSensible;         // Para saber si la transición está sensibilizada o no
     //private static Integer[] transicionSensibleAnterior; // Para comparar si cambió el estado de la transición
     private static Integer[] secuenciaDisparo;
+    public static final int plazas = 15;
+    public static final int transiciones = 12;
+    public static final Integer[] invarianteP1 = {1,2}; // P1 + P2 = 1
+    public static final Integer[] invarianteP2 = {2,3,4}; // P2 + P3 + P4 = 5
+    public static final Integer[] invarianteP3 = {5,6}; // P5 + P6 = 1
+    public static final Integer[] invarianteP4 = {7,8}; // P7 + P8 = 1
+    public static final Integer[] invarianteP5 = {10,11,12,13}; //P10 + P11 + P12 + P13 = 1
+    public static final Integer[] invarianteP6 = {0,2,3,5,8,9,11,12,13,14}; // P0 + P2 + P3 + P5 + P8 + P9 + P11 + P12 + P13 + P14 = 5
+    public static final int inv1 = 1;
+    public static final int inv2 = 5;
+    public static final int inv3 = 1;
+    public static final int inv4 = 1;
+    public static final int inv5 = 1;
+    public static final int inv6 = 5;
+    private Integer[][] invariantes = {invarianteP1, invarianteP2, invarianteP3, invarianteP4, invarianteP5, invarianteP6};
+    private Integer[] invariantesPlazas = {inv1, inv2, inv3, inv4, inv5, inv6};
     private static ArrayList<Integer> transicionesDisparadas;
     private static int disparosT0; //Cantidad de clientes que ingresan
     private static int disparosT2; //Cantidad de veces que gestiono la reserva el gestor1
@@ -19,10 +38,12 @@ public class RedDePetri {
     private static int disparosT6; //Cantidad de reservas confirmadas
     private static int disparosT7; //Cantidad de reservas canceladas
     private static int disparosT11; //Cantidad de clientes que salen
-    private static Long[] timeStamp = new Long[getTransiciones()]; //Tiempos de sensibilizado de cada transición
+    //private static Long[] timeStamp = new Long[Constantes.cantidadTransiciones]; //Tiempos de sensibilizado de cada transición
     //private static long tiempoInicio; //Tiempo de inicio del sistema
     //private static boolean primeraCopia = true; // Para saber si es la primera vez que se llama a sensibilizarT()
     private static Politicas politica = Politicas.getInstance("Balanceada"); // "Balanceada" o "Prioridad".
+    public static final int MAX_CLIENTES = 18;
+    //verificarr--- fin
 
     public RedDePetri(){ 
         System.out.println("Red de Petri creada.");
@@ -38,8 +59,25 @@ public class RedDePetri {
 //-----------------------------------------------------------------------------------------------
     public static void startRedDePetri() {
         System.out.println("Iniciando Red de Petri...");
-        marcadoActual = Constantes.marcadoInicial.clone(); 
+        marcadoActual = marcadoInicial.clone(); 
         System.out.println("Marcado inicial: " + java.util.Arrays.toString(marcadoActual));
+        matrizIncidencia = new Integer[][]{
+            {-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {-1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0},
+            {-1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0, -1, 0, 0, 0, 0, 0, 0},
+            {0, 0, -1, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, -1, 1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, -1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 1, -1, -1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, -1, -1, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0, 0, -1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, -1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, -1}
+        };
         //contadores a 0.
         disparosT0 = 0;
         disparosT2 = 0;
@@ -48,12 +86,12 @@ public class RedDePetri {
         disparosT7 = 0;
         disparosT11 = 0;
         transicionesDisparadas = new ArrayList<>();
-        transicionSensible = new Integer[getTransiciones()];
-        //transicionSensibleAnterior = new Integer[getTransiciones()];
+        transicionSensible = new Integer[transiciones];
+        //transicionSensibleAnterior = new Integer[transiciones];
         //tiempoInicio = System.currentTimeMillis(); // Guarda el tiempo de inicio
         //setCtesTiempo(); // Inicializa los tiempos de sensibilizado en 0
-        secuenciaDisparo = new Integer[getTransiciones()]; 
-        for (int i = 0; i < getTransiciones(); i++) {
+        secuenciaDisparo = new Integer[transiciones]; 
+        for (int i = 0; i < transiciones; i++) {
             transicionSensible[i] = 0;
             //transicionSensibleAnterior[i] = 0;
         }
@@ -66,11 +104,11 @@ public class RedDePetri {
         /*if (!primeraCopia) {
             transicionSensibleAnterior = transicionSensible.clone(); // Copia el estado actual
         }*/
-        for (int j = 0; j < getTransiciones() ; j++) {
+        for (int j = 0; j < transiciones; j++) {
             boolean sensibilizada = true;
             // Verifica para cada plaza si tiene suficientes tokens para disparar la transición
-            for (int i = 0; i < Constantes.plazas ; i++) {
-                if (Constantes.matrizIncidencia[i][j] < 0 && marcadoActual[i] + Constantes.matrizIncidencia[i][j] < 0) {
+            for (int i = 0; i < plazas; i++) {
+                if (matrizIncidencia[i][j] < 0 && marcadoActual[i] + matrizIncidencia[i][j] < 0) {
                     sensibilizada = false;
                     break;
                 }
@@ -99,36 +137,48 @@ public class RedDePetri {
      * @param t numero de transicion 
      */
     public boolean disparar(int t) {
-        if (!isSensible(t)) { //verifica si la transicion esta sensibilizada nuevamente...
-            System.out.println("La transición " + t + " no está sensibilizada.");
-            return false;
-        }        
-        setSecuencia(t);
-        System.out.println("Disparando transición " + t);
-        actualizarMarcado();
-        transicionesDisparadas.add(t);
-        System.out.println("Marcado actual: " + java.util.Arrays.toString(marcadoActual));
-        sensibilizarT();
-        if(t==0) disparosT0++;
-        if(t==11) disparosT11++;
-        if(t==2){
-             disparosT2++;
-             politica.actualizarT2();
+        if(disparosT11>=MAX_CLIENTES) return false;
+        else{
+            if (!isSensible(t)) {
+                System.out.println("La transición " + t + " no está sensibilizada.");
+                return false;
+            }
+
+            if(t==0){
+                System.out.printf("Cantidad de T0 disparadas: %d \n", disparosT0);
+                if(disparosT0>=MAX_CLIENTES){
+                    System.out.printf("\n NO PUEDEN INGRESAR MAS CLIENTES \n");
+                    return true;
+                } 
+                else disparosT0++;
+            }
+                
+            setSecuencia(t);
+            System.out.println("Disparando transición " + t);
+            actualizarMarcado();
+            transicionesDisparadas.add(t);
+            System.out.println("Marcado actual: " + java.util.Arrays.toString(marcadoActual));
+            sensibilizarT();
+            if(t==11) disparosT11++;
+            if(t==2){
+                disparosT2++;
+                politica.actualizarT2();
+            }
+            if(t==3){
+                disparosT3++;
+                politica.actualizarT3();
+            }
+            if(t==6){
+                disparosT6++;
+                politica.actualizarT6();
+            }
+            if(t==7){
+                disparosT7++;
+                politica.actualizarT7();
+            }
+            comprobarInvariantes(t);
+            return true;
         }
-        if(t==3){
-            disparosT3++;
-            politica.actualizarT3();
-       }
-        if(t==6){
-            disparosT6++;
-            politica.actualizarT6();
-       }
-        if(t==7){
-            disparosT7++;
-            politica.actualizarT7();
-       }
-        comprobarInvariantes(t);
-        return true;
     }
     
     public ArrayList<Integer> getTransicionesDisparadas(){
@@ -137,13 +187,13 @@ public class RedDePetri {
     
     /**
      * @brief Actualiza el marcado actual luego de dispararse una transicion
-     * Utiliza la Ecuación fundamental marcadoActual = Constantes. + Constantes.matrizIncidencia * secuenciaDisparo
+     * Utiliza la Ecuación fundamental marcadoActual = marcadoInicial + matrizIncidencia * secuenciaDisparo
      */
     private void actualizarMarcado() {
-        for (int i = 0; i < Constantes.plazas ; i++) {
+        for (int i = 0; i < plazas; i++) {
             int nuevoMarcado = marcadoActual[i]; 
-            for (int j = 0; j < getTransiciones() ; j++) {
-                nuevoMarcado += Constantes.matrizIncidencia[i][j] * secuenciaDisparo[j];
+            for (int j = 0; j < transiciones; j++) {
+                nuevoMarcado += matrizIncidencia[i][j] * secuenciaDisparo[j];
             }
             marcadoActual[i] = nuevoMarcado; 
         }
@@ -155,12 +205,12 @@ public class RedDePetri {
      */
     private void comprobarInvariantes(int t) {    	
     	boolean cumpleTodos = true;
-        for (int i = 0; i < Constantes.invariantes.length; i++) {
+        for (int i = 0; i < invariantes.length; i++) {
             int suma = 0;
-            for (int plaza : Constantes.invariantes[i]) {
+            for (int plaza : invariantes[i]) {
                 suma += marcadoActual[plaza];
             }
-            if (suma != Constantes.invariantesPlazas[i]) {
+            if (suma != invariantesPlazas[i]) {
                 System.out.println("No se cumple el invariante de plaza " + (i + 1) + " al disparar la transición");
                 cumpleTodos = false;
             }
@@ -172,8 +222,8 @@ public class RedDePetri {
      * @brief Devuelve la cantidad de transiciones
      * @return numero de transiciones de la red
      */
-    public static int getTransiciones() {
-    	return Constantes.NumTransiciones;
+    public int getTransiciones() {
+    	return transiciones;
     }
 	    
 	/**
@@ -181,7 +231,7 @@ public class RedDePetri {
 	 * @param t transicion a disparar
 	 */
     private void setSecuencia(int t) {
-    	for (int i = 0; i < getTransiciones(); i++) {
+    	for (int i = 0; i < transiciones; i++) {
             secuenciaDisparo[i] = (i == t) ? 1 : 0;
         }
     }
@@ -224,7 +274,7 @@ public class RedDePetri {
     //  * Al momento de crearse la red, inicia todos los tiempos de sensibilizado en 0
     //  */
     // public static void setCtesTiempo(){
-    //     for (int i = 0; i < getTransiciones(); i++) {
+    //     for (int i = 0; i < Constantes.cantidadTransiciones; i++) {
     //         timeStamp[i] = getTiempoActual();//System.currentTimeMillis();
     //     }
     // }
@@ -262,7 +312,7 @@ public class RedDePetri {
     //  * Actualiza los timeStaps de las transiciones que pasaron de no sensibilizadas a sensibilizadas
     //  */
     // public static void setTimeStamp() {
-    //     for (int i = 0; i < getTransiciones(); i++) {
+    //     for (int i = 0; i < Constantes.cantidadTransiciones; i++) {
     //         if(transicionSensibleAnterior[i] == 0 && transicionSensible[i] == 1){ // Si la transición pasó de no sensibilizada a sensibilizada
     //             timeStamp[i] = getTiempoActual();
     //         }
