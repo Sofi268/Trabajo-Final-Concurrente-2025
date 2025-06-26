a5076c9e383a4046ccf8b87e0961291b10632918
Sofia Castro <116833245+Sofi268@users.noreply.github.com>
2025-06-25 21:44:10 -0300

    Update Main.java

--- src/Agencia/Main.java
+++ src/Agencia/Main.java
@@ -29,7 +29,6 @@ public class Main {
             System.out.println("Error al esperar los hilos.");
         }
                
-        monitor.setFin(); 
         finalizacionHilos(hilos);
 
         for (Thread hilo : hilos) {
